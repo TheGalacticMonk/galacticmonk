e27ac999eff0e67ab133b61521f9bd6610508bec
Galactic Monk <mail@jasonclee.net>
2026-08-13 02:10:30 -0700

    Update website

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import SectionHeading from "@/components/SectionHeading";
 import ContactForm from "@/components/ContactForm";
 import Sparkle from "@/components/Sparkle";
-import Moon from "@/components/Moon";
 import {
   MailIcon,
   InstagramIcon,
@@ -28,8 +27,7 @@ export default function ContactPage() {
             description={
               <>
                 Tell me a bit about what you&apos;re imagining and
-                I&apos;ll get back to you within a couple of moons.{" "}
-                <Moon className="inline-block h-4 w-4 -translate-y-px text-gold" />
+                I&apos;ll get back to you within a couple of Suns & Moons. ☀️🌙
               </>
             }
           />
