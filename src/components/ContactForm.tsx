be8bb57f4e65bd222a6b60dfc63a318acee86276
Galactic Monk <mail@jasonclee.net>
2026-08-13 20:54:39 -0700

    Update

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -89,7 +89,7 @@ export default function ContactForm() {
               type="text"
               name="budget_timeline"
               className="input"
-              placeholder="e.g. $2-5k"
+              placeholder="e.g. $1-5k"
             />
           </Field>
         </div>
